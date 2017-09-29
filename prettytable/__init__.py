a78ff9b04cd8ef5b3f79418d9a07e170e547db82
Brano Zarnovican <zarnovican@gmail.com>
2017-09-29 15:15:41 +0200

    Added PLAIN_COLUMNS to top namespace
    Example from documentation works only for MSWORD_FRIENDLY style
    
    > from prettytable import MSWORD_FRIENDLY       # works
    > from prettytable import PLAIN_COLUMNS         # broken
    
    This commit fixes import for PLAIN_COLUMNS style

--- prettytable/__init__.py
+++ prettytable/__init__.py
@@ -33,5 +33,5 @@
 __version__ = "0.9.2"
 
 from .prettytable import PrettyTable
-from .prettytable import ALL, HEADER, MSWORD_FRIENDLY, NONE
+from .prettytable import ALL, HEADER, MSWORD_FRIENDLY, NONE, PLAIN_COLUMNS
 from .factory import from_csv, from_db_cursor, from_html, from_html_one
