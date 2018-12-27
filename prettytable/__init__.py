7a38ae0efd2f0c79e33e45769a481ecda9b778b9
Thibault Ducret <thibault.ducret@gmail.com>
2018-12-27 22:01:31 +0100

    Update to push 0.9.3 on Pypi

--- prettytable/__init__.py
+++ prettytable/__init__.py
@@ -30,7 +30,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 
 from .prettytable import PrettyTable
 from .prettytable import ALL, HEADER, MSWORD_FRIENDLY, NONE, PLAIN_COLUMNS
